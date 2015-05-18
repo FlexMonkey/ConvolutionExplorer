e24223959556710b5ccddfdb830eeb3578f912b2
Simon Gladman <simongladman@Simons-MacBook-Pro-2.local>
2015-05-18 20:36:49 +0100

    Initial Commit

---
+++ ConvolutionExplorer/KernelEditor.swift
@@ -0,0 +1,188 @@
+//
+//  KernelEditor.swift
+//  ConvolutionExplorer
+//
+//  Created by Simon Gladman on 18/05/2015.
+//  Copyright (c) 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class KernelEditor: UIView
+{
+    let mainGroup = SLVGroup()
+    let topSpacer = SLSpacer(percentageSize: nil, explicitSize: nil)
+    let bottomSpacer = SLSpacer(percentageSize: nil, explicitSize: nil)
+    
+    var cells = [KernelEditorCell]()
+    
+    required init()
+    {
+        super.init(frame: CGRectZero)
+
+        mainGroup.children.append(topSpacer)
+        mainGroup.margin = 7
+        
+        for i in 0 ... 6
+        {
+            let row = KernelEditorRow(rowNumber: i, kernelEditor: self)
+            mainGroup.children.append(row)
+        }
+        
+        mainGroup.children.append(topSpacer)
+        
+        addSubview(mainGroup)
+    }
+
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var kernelSize: KernelSize = KernelSize.ThreeByThree
+    {
+        didSet
+        {
+            for cell in cells
+            {
+                switch kernelSize
+                {
+                case .ThreeByThree:
+                    cell.enabled = cell.rowNumber >= 2 && cell.rowNumber <= 4 && cell.columnNumber >= 2 && cell.columnNumber <= 4
+                case .FiveByFive:
+                    cell.enabled = cell.rowNumber >= 1 && cell.rowNumber <= 5 && cell.columnNumber >= 1 && cell.columnNumber <= 5
+                case .SevenBySeven:
+                    cell.enabled = true
+                }
+            }
+        }
+    }
+    
+    var touchedCells = [KernelEditorCell]()
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        super.touchesBegan(touches, withEvent: event)
+        touchedCells = [KernelEditorCell]()
+        
+        if let touch = touches.first as? UITouch
+        {
+            let obj = (hitTest(touch.locationInView(self), withEvent: event))
+            
+            if let obj = obj as? KernelEditorCell where obj.enabled
+            {
+                obj.selected = !obj.selected
+                touchedCells.append(obj)
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        super.touchesMoved(touches, withEvent: event)
+        
+        if let touch = touches.first as? UITouch
+        {
+            let obj = (hitTest(touch.locationInView(self), withEvent: event))
+            
+            if let obj = obj as? KernelEditorCell where obj.enabled && find(touchedCells, obj) == nil
+            {
+                obj.selected = !obj.selected
+                touchedCells.append(obj)
+            }
+        }
+    }
+    
+    override func layoutSubviews()
+    {
+        topSpacer.explicitSize = frame.width / 4
+        bottomSpacer.explicitSize = frame.width / 4
+        mainGroup.frame = frame
+    }
+}
+
+class KernelEditorRow: SLHGroup
+{
+    var kernelEditor: KernelEditor
+    
+    required init(rowNumber: Int, kernelEditor: KernelEditor)
+    {
+        self.kernelEditor = kernelEditor
+        
+        super.init()
+        
+        margin = 7
+        
+        for i in 0 ... 6
+        {
+            let cell = KernelEditorCell(rowNumber: rowNumber, columnNumber: i, kernelEditor: kernelEditor)
+            children.append(cell)
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    required init()
+    {
+        fatalError("init() has not been implemented")
+    }
+}
+
+class KernelEditorCell: SLLabel
+{
+    var kernelEditor: KernelEditor
+    var rowNumber: Int
+    var columnNumber: Int
+    
+    required init(rowNumber: Int, columnNumber: Int, kernelEditor: KernelEditor)
+    {
+        self.kernelEditor = kernelEditor
+        self.rowNumber = rowNumber
+        self.columnNumber = columnNumber
+        
+        super.init(frame: CGRectZero)
+     
+        userInteractionEnabled = true
+        
+        textAlignment = NSTextAlignment.Center
+
+        layer.backgroundColor = UIColor.lightGrayColor().CGColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 3
+        text = "\(rowNumber):\(columnNumber)"
+        
+        kernelEditor.cells.append(self)
+    }
+    
+    var selected: Bool = false
+    {
+        didSet
+        {
+            layer.backgroundColor = selected ? UIColor.blueColor().CGColor : UIColor.lightGrayColor().CGColor
+            textColor = selected ? UIColor.whiteColor() : UIColor.blackColor()
+        }
+    }
+    
+    override var enabled: Bool
+    {
+        didSet
+        {
+            alpha = enabled ? 1 : 0.5
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum KernelSize: String
+{
+    case ThreeByThree = "3 x 3"
+    case FiveByFive = "5 x 5"
+    case SevenBySeven = "7 x 7"
+}
--- ConvolutionExplorer/ViewController.swift
+++ ConvolutionExplorer/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    let mainGroup = SLVGroup()
+    let workspace = SLHGroup()
+    let toolbar = SLHGroup()
+    let imageView = UIImageView()
+    let kernelEditor = KernelEditor()
+    let valueSlider = UISlider()
+    
+    let kernelSizeSegmentedControl = UISegmentedControl(items: [KernelSize.ThreeByThree.rawValue, KernelSize.FiveByFive.rawValue, KernelSize.SevenBySeven.rawValue])
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let image = UIImage(named: "image.jpg")
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        
+        kernelSizeSegmentedControl.addTarget(self, action: "kernelSizeChange", forControlEvents: UIControlEvents.ValueChanged)
+        
+        workspace.children = [kernelEditor, imageView]
+        
+        toolbar.children = [valueSlider, kernelSizeSegmentedControl]
+        toolbar.explicitSize = 40
+        
+        mainGroup.children = [workspace, toolbar]
+        view.addSubview(mainGroup)
+        
+        kernelSizeSegmentedControl.selectedSegmentIndex = 0
+        kernelSizeChange()
+    }
+    
+    func kernelSizeChange()
+    {
+        if let kernelSize = KernelSize(rawValue: kernelSizeSegmentedControl.titleForSegmentAtIndex(kernelSizeSegmentedControl.selectedSegmentIndex)!)
+        {
+            kernelEditor.kernelSize = kernelSize
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews()
+    {
+        let top = topLayoutGuide.length
+        let bottom = bottomLayoutGuide.length
+        
+        mainGroup.frame = CGRect(x: 0, y: top, width: view.frame.width, height: view.frame.height - top - bottom).rectByInsetting(dx: 5, dy: 0)
     }
-
 
 }
 
